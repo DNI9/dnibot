46c6703c04435a776c0143db6177f271319ae98a
DNI9 <itsr9boi@gmail.com>
2019-07-23 14:45:30 +0530

    Update spammer.py

--- userbot/modules/spammer.py
+++ userbot/modules/spammer.py
@@ -23,8 +23,8 @@ async def tmeme(e):
 async def spammer(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         message = e.text
-        counter = int(message[15:18])
-        spam_message = str(e.text[18:])
+        counter = int(message[6:8])
+        spam_message = str(e.text[8:])
         await asyncio.wait([e.respond(spam_message) for i in range(counter)])
         await e.delete()
         if LOGGER:
